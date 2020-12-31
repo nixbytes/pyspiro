f3cb03f146b69bb79a266a9d019ae564b29bcc3c
linuxbytes <real8686@gmail.com>
2020-12-31 09:57:28 -0500

    sert function

--- pyspiro/spiro.py
+++ pyspiro/spiro.py
@@ -1,5 +1,8 @@
 # A class that draws a Spirograph
 import turtle
+from datetime import datetime
+from fractions import gcd
+
 
 class Spiro:
     def __init__(self, xc, yc, col, R, r, l) -> None:
@@ -16,3 +19,22 @@ class Spiro:
         # initalize the drawing
         self.restart()
 
+    def setparams(self, xc, yc, col, R, r, l):
+        self.xc = xc
+        self.yc = yc
+        self.col = col
+        self.R = R
+        self.r = r
+        self.l = l
+        """
+        Reduce r/R to the smallest form by Dividing with the
+        GCD (Greatest Command Divisor )
+        """
+        gcd_val = gcd(self.r, self.R)
+        self.nRot = self.r // self.R
+
+        # Ratio of Radii
+
+        self.k = r / float(R)
+        self.t.color(*col)
+        self.a = 0
