d794c50a4bfbe7c9b561937810bbf8b1c18fb1e8
linuxbytes <real8686@gmail.com>
2021-01-18 18:56:19 -0500

    add Spiro def

--- pyspiro/spiroanimator.py
+++ pyspiro/spiroanimator.py
@@ -1,3 +1,24 @@
-
+# a class for animating spirograph
 class Spiro_Animator:
-    pass
+    def __init__(self, N):
+        # set the timer value in milliseconds and get the window dimensions
+        self.width = turtle.window_width()
+        self.wheight = turtle.window_height
+        self.spiros = []
+        for i in range(N):
+            # generate random parameters and set the spiro parameters
+            rparams = self.genRandomParams
+            spiro = Spiro(*rparams)
+            self.spiros.append(spiro)
+            turtle.ontimer(self.update, self.deltaT)
+
+    def genRandomParams(self):
+        width, height = self.width, self.heigth
+        R = random.randint(50, min(width, height) // 2)
+        r = random.randint(10, 9 * R // 10)
+        l = random.uniform(0.1, 0.9)
+        xc = random.randint(-width // 2, width // 2)
+        yc = random.randint(-height // 2, height // 2)
+
+        col = (random.random(), random.random(), random.random())
+        return (xc, yc, col, R, r, l)
