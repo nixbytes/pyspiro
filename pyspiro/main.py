4d7fe9286ae2922b455cd9b0abd464199bafaa66
linuxbytes <real8686@gmail.com>
2021-01-27 20:20:56 -0500

    add cli arg

--- pyspiro/main.py
+++ pyspiro/main.py
@@ -1,6 +1,46 @@
+import spiro
+import spiroanimator
+
 
 def main():
-    pass
+    print("generating spirograph...")
+    descStr = """The program draws Spiropgraph using the Turtle Modules.
+    When run with no arguments, this program draws random Spirographs.
 
-if (__name__ =='__main__'):
+    Terminology:
+
+    R: radius of outer circle
+    r: radius of inner circle
+    l: ratio of hole distance to r
+
+
+    """
+    parse = argparse.ArgumentParser(description=descStr)
+
+    parser.add_argument(
+        "--sparam",
+        nargs=3,
+        dest="sparams",
+        required=False,
+        help="The three arguments in sparams: R, r, l.",
+    )
+
+    arg = parser.parse_args()
+
+    turtle.setup(width=0.8)
+
+    turtle.shape("turtle")
+
+    turtle.title("Spirograph")
+
+    turtle.onkey(saveDrawing, "s")
+
+    turtle.listen()
+
+    turtle.hideturtle()
+
+    turtle.mainloop()
+
+
+if __name__ == "__main__":
     main()
